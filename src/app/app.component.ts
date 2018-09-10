f22b37b940153d9c1eedcb21a1c372cb49b4e13d
VinceGonzales <vince.gonzales@hotmail.com>
2018-09-10 08:47:56 -0700

    accessing child members with ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // access the child as a variable
+  @ViewChild(CounterComponent)
+  counterComponent: CounterComponent;
+
   title = 'app';
   comments = [
   { author: 'Jay Kay', content: 'TypeScript makes Angular awesome' },
