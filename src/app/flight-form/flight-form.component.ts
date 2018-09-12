8c105b6df3705763e39763146e54e649a98e8fa8
VinceGonzales <vince.gonzales@hotmail.com>
2018-09-12 10:11:34 -0700

    add to form

--- src/app/flight-form/flight-form.component.ts
+++ src/app/flight-form/flight-form.component.ts
@@ -8,6 +8,9 @@ import { Flight } from '../flight';
 })
 export class FlightFormComponent implements OnInit {
 	flightModel: Flight;
+	cities: Array<string> = [
+	'Lagos', 'Mumbai', 'New York', 'London', 'Nairobi'
+	];
 	constructor() {
 		this.flightModel = new Flight('','','','',0,new Date(),0,0,new Date());
 	}
